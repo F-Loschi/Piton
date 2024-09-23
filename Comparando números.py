8c8dbbdc2f77d9705cd298a8eecb9fdbf807efd8
Felipe Silva Loschi <166191574+F-Loschi@users.noreply.github.com>
2024-09-23 11:21:50 -0300

    Create Comparando números.py

---
+++ Comparando números.py
@@ -0,0 +1,9 @@
+num1 = float(input('Digite o 1° número: '))
+num2 = float(input('Digite o 2° número: '))
+
+if num1 > num2:
+    print(f'O primeiro número é maior: {num1}')
+elif num2 > num1:
+    print(f'O segundo número é maior: {num2}')
+else: # Caso os números sejam iguais
+    print('Os dois números são iguais.')
