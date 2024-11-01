2a9f8dedd27cc1f8eea1cc93d566a412b7a55c34
Felipe Silva Loschi <166191574+F-Loschi@users.noreply.github.com>
2024-11-01 20:55:46 -0300

    Create Pegando coisa de tupla.py

---
+++ Pegando coisa de tupla.py
@@ -0,0 +1,7 @@
+lista_de_tuplas = [('Pedro', 1.74, 81), ('Júlia', 1.65, 67), ('Otávio', 1.81, 83)]
+
+terceiro_elemento = []
+
+for i in lista_de_tuplas:
+    terceiro_elemento.append(i[2])
+print(terceiro_elemento)
