971c7816bf7ede8eadcbdc070fbf9b2305383333
Felipe Silva Loschi <166191574+F-Loschi@users.noreply.github.com>
2024-11-01 22:55:18 -0300

    Create Dict comprehension.py

---
+++ Dict comprehension.py
@@ -0,0 +1,6 @@
+meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360]
+
+pagar = {meses[i]: despesa[i] for i in range(0, len(meses))}
+
+print(pagar)
